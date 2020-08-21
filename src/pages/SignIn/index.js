ce1ee775f7a194bbb4f292f689f70f574856ff68
Gabriel Barreto <gabriel.barret015@hotmail.com>
2020-08-20 21:20:30 -0300

    customização e criação da tela de login.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,28 +1,106 @@
 import React from "react";
-import { Input, Grid, Box, Typography } from "@material-ui/core";
+import { Grid, Box, Typography, Avatar, Button, Link } from "@material-ui/core";
 
-import useStyles from "./styles";
+import { useStyles, StyledTextField } from "./styles";
 import LogoDev from "../../assets/dev.svg";
+
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 function SignIn() {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
-      <div className={classes.left}>
-        <img src={LogoDev} alt="logo" className={classes.imgLogo} />
-        <Typography className={classes.title}>
-          <strong>Faça seu login na plataforma</strong>
-        </Typography>
-      </div>
-      <div className={classes.right}>
-        <form className={classes.form}>
-          <h4>Acesso</h4>
-          <input type="text" />
-          <input type="text" />
-        </form>
-      </div>
+      <Grid container>
+        <Grid
+          item
+          container
+          md={6}
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <img src={LogoDev} alt="logo" className={classes.imgLogo} />
+          <Typography className={classes.title}>
+            <strong>Faça seu login na plataforma</strong>
+          </Typography>
+        </Grid>
+
+        <Grid
+          item
+          container
+          md={6}
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            bgcolor="#202024"
+            p={5}
+            borderRadius={8}
+          >
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <form className={classes.form}>
+              <StyledTextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullwidth
+                id="email"
+                name="email"
+                label="E-mail"
+                autoFocus
+                color="secondary"
+              />
+
+              <StyledTextField
+                id="standard-adornment-password"
+                variant="outlined"
+                required
+                fullwidth
+                color="secondary"
+                label="Senha"
+                name="password"
+                margin="normal"
+                disableUnderline={false}
+                type="password"
+              />
+              <Grid container>
+                <Grid item>
+                  <Link
+                    component="button"
+                    variant="body2"
+                    className={classes.register}
+                    underline="none"
+                  >
+                    {" "}
+                    Não tem uma conta? Registre-se
+                  </Link>
+                </Grid>
+              </Grid>
+
+              <Button
+                fullWidth
+                variant="contained"
+                type="submit"
+                color="secondary"
+                className={classes.button}
+              >
+                Entrar
+              </Button>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
 export default SignIn;
+//3311-5676
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,20 +1,12 @@
+import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     height: "100vh",
     background: "#121214",
-  },
-
-  left: {
-    color: "white",
-
-    display: "flex",
-    flexBasis: "58%",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
   },
 
   title: {
@@ -28,21 +20,63 @@ const useStyles = makeStyles((theme) => ({
     height: 300,
   },
 
-  right: {
-    color: "white",
-
-    display: "flex",
-    flexBasis: "42%",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-
   form: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
+
+  avatar: {
+    background: "#7000f2",
+    marginBottom: 30,
+  },
+
+  icon: {
+    color: "#7000f2",
+  },
+
+  button: {
+    height: 50,
+    marginTop: 30,
+    transition: "0.4s",
+  },
+
+  register: {
+    color: "#7000f2",
+    fontWeight: "bold",
+    fontSize: 17,
+    transition: "0.4s",
+    marginTop: 8,
+
+    "&:hover": {
+      color: "#9034fa",
+    },
+  },
 }));
 
-export default useStyles;
+const StyledTextField = styled(TextField)`
+  background: #121214;
+  border-radius: 8px;
+  label {
+    color: #fff;
+  }
+  label.Mui-focused {
+    color: #fff;
+  }
+  .MuiOutlinedInput-root {
+    color: #fff;
+    width: 350px;
+    fieldset {
+      border-color: none;
+    }
+    &:hover fieldset {
+      border-color: transparent;
+      outline-color: transparent;
+    }
+    &.Mui-focused fieldset {
+      border-color: #7000f2;
+    }
+  }
+`;
+
+export { useStyles, StyledTextField };
